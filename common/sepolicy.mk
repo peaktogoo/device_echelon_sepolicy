00894eda493640bfb76d812720bc7ddb8fc1d8f6
peaktogoo <41993474+peaktogoo@users.noreply.github.com>
2019-01-28 10:08:05 +0700

    Update sepolicy.mk

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -4,10 +4,10 @@
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/gzosp/sepolicy/common/public
+    device/echelon/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/gzosp/sepolicy/common/private
+    device/echelon/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/gzosp/sepolicy/common/vendor
+    device/echelon/sepolicy/common/vendor
